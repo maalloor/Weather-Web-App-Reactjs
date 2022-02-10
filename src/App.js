d802ca6cf049b9846be04c68e102ccdd5f513d80
maalloor <maalloor@espol.edu.ec>
2022-02-10 08:00:32 -0500

    Obtaining data corresponding to the city entered in the search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: '9dee6ea9c3b05ecdc95fdfed5d03b9a6',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
 
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = df => {
+    if (df.key === 'Enter')
+    {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
   /*Method to build dates*/
   const dateFormat = (df) => {
     let days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
@@ -22,7 +37,11 @@ function App() {
   <div className="app">
     <main>
       <div className="search">
-        <input type="text" className="search-input" placeholder="Buscar la región"/>
+        <input type="text" className="search-input" placeholder="Buscar la región"
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
       </div>
       <div className="location-section">
         <div className="location">
