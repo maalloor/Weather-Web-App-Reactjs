4375e515f681773c5db56fc7703c817b8e0e0cb0
maalloor <maalloor@espol.edu.ec>
2022-02-09 22:17:06 -0500

    Adding assets (hot and cold) and css code for the main section of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+const api = {
+  key: '9dee6ea9c3b05ecdc95fdfed5d03b9a6',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="app">
+    <main>
+      <div className="search">
+        <input type="text" className="search-input" placeholder="Buscar la región"/>
+      </div>
+    </main>
+  </div>
   );
 }
 
