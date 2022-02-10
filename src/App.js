95d32e60a498bc9db44823756a601558c7a54880
maalloor <maalloor@espol.edu.ec>
2022-02-10 07:30:44 -0500

    Adding the location, date, temperature and weather sections in the APP, with the respective CSS effects

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+
+  /*Method to build dates*/
+  const dateFormat = (df) => {
+    let days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+    let months = ["Enero", "Febrero", "Marzo", "Abril", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    let day = days[df.getDay()];
+    let date = df.getDate();
+    let month = months[df.getMonth()];
+    let year = df.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  }
+
   return (
   <div className="app">
     <main>
       <div className="search">
         <input type="text" className="search-input" placeholder="Buscar la región"/>
+      </div>
+      <div className="location-section">
+        <div className="location">
+          New York City, US
+        </div>
+        <div className="date">
+          {dateFormat(new Date())}
+        </div>
+      </div>
+      <div className="weather-section">
+        <div className="temperature">
+          15°C
+        </div>
+        <div className="weather">
+          Sunny
+        </div>
       </div>
     </main>
   </div>
